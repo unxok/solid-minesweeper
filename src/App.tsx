0b24339ceb6e2c21ddbc2a734a977ac70de2d98b
unxok <me@unxok.com>
2024-07-17 22:32:02 -0700

    twcss and shadcn set up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+
+import { Button } from "./components/ui/button";
+
+const App: Component = () => {
+	return (
+		<div>
+			<p>uwu</p>
+			<Button>click me</Button>
+		</div>
+	);
+};
+
+export default App;
